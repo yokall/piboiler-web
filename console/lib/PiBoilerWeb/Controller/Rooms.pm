26c4b1d4941604bb808d5b50cc9d192edcfa4434
Developer <developer@debian>
2016-03-14 16:33:53 +0000

    templates for models started/Index page fleshed out/add room page functioning

--- console/lib/PiBoilerWeb/Controller/Rooms.pm
+++ console/lib/PiBoilerWeb/Controller/Rooms.pm
@@ -1,14 +1,34 @@
 package PiBoilerWeb::Controller::Rooms;
 use Mojo::Base 'Mojolicious::Controller';
+use Data::Dumper;
+
+use PiBoilerWeb::Model::Rooms;
 
 sub add {
     my $self = shift;
+
+    warn "WARN";
 }
 
 sub add_post {
     my $self = shift;
 
+    warn "POST";
+
+    my $params = $self->req->params->to_hash();
+    warn Dumper($params);
+
     return $self->redirect_to('/');
+}
+
+sub delete {
+
+}
+
+sub edit {
+    my $self = shift;
+
+    my $id = $self->param('id');
 }
 
 1;
