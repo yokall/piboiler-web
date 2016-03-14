2c90e93b3fc7de7a5e00e363d9feb5965f3343dc
Mellissa White <mellissa.white@civica.co.uk>
2016-03-14 17:18:47 +0000

    Adding in services to find all temperature details.

--- lib/PiBoiler/Model/Rooms.pm
+++ lib/PiBoiler/Model/Rooms.pm
@@ -4,11 +4,15 @@ use PiBoiler::Connect;
 
 my $dbh = PiBoiler::Connect::dbconnect();
 
-my $query_room_id_sql = "SELECT _id FROM room WHERE sensor_id=?";
-my $query_room_id_sth = $dbh->prepare($query_room_id_sql);
+my $query_room_id_by_sensor_sql = "SELECT _id FROM room WHERE sensor_id=?";
+my $query_room_id_by_sensor_sth = $dbh->prepare($query_room_id_by_sensor_sql);
+
+my $query_room_id_by_name_sql = "SELECT _id FROM room WHERE room_name=?";
+my $query_room_id_by_name_sth = $dbh->prepare($query_room_id_by_name_sql);
 
 my $create_room_sql = "INSERT INTO room SET room_name=?, sensor_id=?";
 my $create_room_sth = $dbh->prepare($create_room_sql);
+
 
 sub create_room {
     my $room_name = shift;
@@ -16,7 +20,7 @@ sub create_room {
 
     $create_room_sth->execute($room_name, $sensor_id);
 
-    my $id = mysql_insert_id();
+    my $id = $create_room_sth->(mysql_insertid);
 
     my $error;
 
@@ -28,12 +32,12 @@ sub create_room {
     return ($id, $error);
 }
 
-sub query_room {
+sub query_room_by_sensor {
     my $sensor_id = shift;
 
-    $query_room_id_sth->execute($sensor_id);
+    $query_room_id_by_sensor_sth->execute($sensor_id);
 
-    my $room_id = $query_room_id_sth->fetchrow_array();
+    my $room_id = $query_room_id_by_sensor_sth->fetchrow_array();
 
     my $error;
 
@@ -43,4 +47,22 @@ sub query_room {
 
     return ($room_id, $error);
 }
+
+sub query_room_by_name {
+    my $room_name = shift;
+
+    $query_room_id_by_name_sth->execute($room_name);
+
+    my $room_id = $query_room_id_by_name_sth->fetchrow_array();
+
+    my $error;
+
+    unless($room_id) {
+        $error = "Cannot Find Room for $room_name";
+    }
+
+    return ($room_id, $error);
+
+}
+
 1;
--- lib/PiBoiler/Model/Temperature.pm
+++ lib/PiBoiler/Model/Temperature.pm
@@ -10,6 +10,12 @@ my $dbh = PiBoiler::Connect::dbconnect();
 my $create_temperature_sql = "INSERT INTO temperature SET timestamp=?, temperature=?, sensor_id=?, room_id=?";
 my $create_temperature_sth = $dbh->prepare($create_temperature_sql);
 
+my $query_temperature_sql = "SELECT timestamp, temperature FROM temperature where room_id=?";
+my $query_temperature_sth = $dbh->prepare($query_temperature_sql);
+
+my $query_all_temperature_sql = "SELECT t.timestamp, t.temperature, tp.description, dp.start_time, dp.end_time, dp.day, dp.target_temperature  FROM temperature t, temperature_plan tp, day_part dp where room_id=?";
+my $query_all_temperature_sth = $dbh->prepare($query_all_temperature_sql);
+
 sub create_temperature {
     my $temperature = shift;
     my $date_time = shift;
@@ -18,7 +24,7 @@ sub create_temperature {
 
     $create_temperature_sth->execute($date_time, $temperature, $sensor_id, $room_id);
 
-    my $id = mysql_insert_id();
+    my $id = $create_temperature_sth->(mysql_insertid);
     my $error;
 
     unless($id) {
@@ -26,6 +32,65 @@ sub create_temperature {
     }
 
     return ($id, $error);
+}
+
+
+sub query_temperature_details {
+    my $room_id = shift;
+
+    $query_temperature_sth->execute($room_id);
+
+    my ($timestamp, $temperature) = $query_temperature_sth->fetchrow_array();
+
+    my $error;
+
+    unless($timestamp and $temperature) {
+        $error = "Could not get temperature details";
+    }
+
+    my %data_ref = (
+        'timestamp' => $timestamp,
+        'temperature' => $temperature,
+    );
+
+    return (\%data_ref, $error);
+}
+
+sub query_temperature_details {
+    my $room_id = shift;
+
+    $query_temperature_sth->execute($room_id);
+
+    my ($timestamp, $temperature) = $query_temperature_sth->fetchrow_array();
+
+    my $error;
+
+    unless($timestamp and $temperature) {
+        $error = "Could not get temperature details";
+    }
+
+    my %data_ref = (
+        'timestamp' => $timestamp,
+        'temperature' => $temperature,
+    );
+
+    return (\%data_ref, $error);
+}
+
+sub query_all_temperature_details {
+    my $room_id = shift;
+
+    $query_all_temperature_sth->execute($room_id);
+
+    my $temperature_info = $query_temperature_sth->fetchrow_hashref();
+
+    my $error;
+
+    unless($temperature_info) {
+        $error = "Could not get temperature details";
+    }
+
+    return (\temperature_info, $error);
 }
 
 1;
